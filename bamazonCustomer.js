e7ef832c65a7e37974b4a84c0ba4bc5314af2826
Ben Mizelle <Ben Mizelle>
2017-04-08 23:16:51 -0400

    calling it a night

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,10 +2,11 @@
 var mysql      = require('mysql');
 // connects to the Bamazon db
 var connection = mysql.createConnection({
-    host     : "localhost",
-    user     : "root",
-    password : "newcastle22",
-    database : "Bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "newcastle22",
+    database: "Bamazon"
 });
 // begin connection
 connection.connect();
@@ -18,11 +19,11 @@ connection.end();
 var prompt = require('prompt');
 
 prompt.start();
+
 // Get two properties from the user: username and email
 prompt.get(['username', 'email'], function (err, result) {
 
     // Log the results.
-
     console.log('Command-line input received:');
     console.log('  username: ' + result.username);
     console.log('  email: ' + result.email);
