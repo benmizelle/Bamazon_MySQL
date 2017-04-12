6f9122ec37ccb5926923f56b042bcb1ef1c5e251
Ben Mizelle <Ben Mizelle>
2017-04-12 15:30:04 -0400

    general save: working on db query

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,15 +17,36 @@ connection.connect();
 prompt.start();
 
 // Get two properties from the user: username and email
-prompt.get(["What are you lookig for?", "How many?"], function (err, result) {
+prompt.get(["What are you looking for today?", "How many?"], function (err, result) {
 
     // Log the results.
     console.log("Command-line input received:");
     console.log("  item: " + result.item_id);
-    console.log("  quantity: " + result.stock_quantity);
-});
+    console.log("  quantity: " + result.quantity);
+}).then(function(answer) {
+    // function to pull db info
+    var checkProducts = function() {
+        // query the database for product availability
+        connection.query("SELECT stock_quantity FROM products", function(err, results){
+            if (err) throw err;
 
-// Queries and such here
+            var productQuantity = results[i];
+
+            for (var i = 0; i < results.length; i++) {
+                if (productQuantity > 0) {
+                    console.log("Added to cart.");
+                }
+                else {
+                    console.log("Sorry, we are currently out of stock. Please make another selection.");
+                }
+            }
+
+
+
+        });
+    }
+
+}
 
 // end connection
 connection.end();
