78f62aa0ccaf6a16d6ad081c6ab7d8f71dcab6d0
Ben Mizelle <Ben Mizelle>
2017-04-12 13:56:12 -0400

    .gitignore file added, prompt working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
 // require Bamazon db
-var mysql      = require('mysql');
+var mysql      = require("mysql");
+
+var prompt = require("prompt");
+
 // connects to the Bamazon db
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,20 +14,21 @@ var connection = mysql.createConnection({
 // begin connection
 connection.connect();
 
+prompt.start();
+
+// Get two properties from the user: username and email
+prompt.get(["What aare you lookig for?", "How many?"], function (err, result) {
+
+    // Log the results.
+    console.log("Command-line input received:");
+    console.log("  item: " + result.item_id);
+    console.log("  quantity: " + result.quantity);
+});
+
 // Queries and such here
 
 // end connection
 connection.end();
 
-var prompt = require('prompt');
 
-prompt.start();
 
-// Get two properties from the user: username and email
-prompt.get(['username', 'email'], function (err, result) {
-
-    // Log the results.
-    console.log('Command-line input received:');
-    console.log('  username: ' + result.username);
-    console.log('  email: ' + result.email);
-});
